acc51aff9e0d243727f06afd8ac6c7c56de8be08
Andreas Remar <andreas.remar@gmail.com>
2022-11-23 19:08:36 +0100

    Make a prettier page

---
+++ build.py
@@ -0,0 +1,32 @@
+import markdown
+import os
+
+posts = os.listdir("posts")
+posts.sort()
+posts.reverse()
+
+def read_file(path):
+    with open(path) as f:
+        return f.read()
+
+page = []
+
+markdown_posts = []
+
+for post in posts:
+    markdown_posts.append(markdown.markdown(read_file(f"posts/{post}")))
+
+page.append(read_file("template/pre.html"))
+
+for post in markdown_posts:
+    page.append("<article class=\"textretty\">")
+    page.append(post)
+    page.append("</article><hr/>")
+
+page.append(read_file("template/post.html"))
+
+with open("index.html", "w") as f:
+    f.write("\n".join(page))
+
+print("\n".join(page))
+        #markdown.
--- build.sh
+++ build.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-posts=$(ls -r posts)
-
-p=(posts/*)
-
-first="${p[0]}"
-
-file_list="template/pre.html"
-
-for f in ${posts}
-do
-    file_list+=" posts/$f"
-    if [[ "posts/$f" == "$first" ]]
-    then
-	break
-    fi
-    file_list+=" template/line.txt"
-done
-
-file_list+=" template/post.html"
-
-cat $file_list
+python build.py
